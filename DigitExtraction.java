711e07530ba935c78d0474efa76d0ae69338ab7f
HardikJuneja10 <hardikjuneja10@gmail.com>
2026-08-03 08:27:19 +0530

    Update DigitExtraction.java

--- DigitExtraction.java
+++ DigitExtraction.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
-public class DigitExtraction {
-    public static void Digit(int num){
+public class DigitExtraction
+    {
+    public static void Digit(int num)
+        {
         int hundreds = num/100;
+            
         int tens = (num/10)%10;
         int unit = num%10;
         System.out.println("Hundreds: "+hundreds);
         System.out.println("Tens : "+tens);
         System.out.println("Unit : "+unit);
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+        {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         Digit(num);
